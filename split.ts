5d04327d56512802847fed837d925875cc775fad
Barbara Odozi <barbs.nene@gmail.com>
2023-09-01 10:20:29 +0100

    add

---
+++ split.ts
@@ -0,0 +1,20 @@
+function abbreviateName(name: string): string {
+  // Split the name into words
+  const words: string[] = name.split(" ");
+
+  // Check if there are at least two words
+  if (words.length >= 2) {
+    // Take the first character of each word, convert to uppercase, and join with a dot
+    const initials: string = words
+      .map((word) => word[0].toUpperCase())
+      .join(".");
+    return initials;
+  }
+
+  // If there are not enough words, return the input as is
+  return name;
+}
+
+console.log(abbreviateName("Sam Harris")); // Output: "S.H"
+console.log(abbreviateName("patrick feeney")); // Output: "P.F"
+console.log(abbreviateName("John")); // Output: "John" (Not enough words)
