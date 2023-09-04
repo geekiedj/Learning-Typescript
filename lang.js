e070f84614247aeba88d4c556d2d07481df888b2
Barbara Odozi <barbs.nene@gmail.com>
2023-09-04 10:12:23 +0100

    add

---
+++ lang.js
@@ -0,0 +1,26 @@
+function welcome (language ) {
+    const langdb = {
+    english: "Welcome",
+    czech: "Vitejte",
+    danish: "Velkomst",
+    dutch: "Welkom",
+    estonian: "Tere tulemast",
+    finnish: "Tervetuloa",
+    flemish: "Welgekomen",
+    french: "Bienvenue",
+    german: "Willkommen",
+    irish: "Failte",
+    italian: "Benvenuto",
+    latvian: "Gaidits",
+    lithuanian: "Laukiamas",
+    polish: "Witamy",
+    spanish: "Bienvenido",
+    swedish: "Valkommen",
+    welsh: "Croeso",
+    };
+    const normlang = language.toLowerCase ();
+    if langdb.hasOwnProperty (normlang)
+    langdb[normlang]
+
+    else return langdb.english
+}
